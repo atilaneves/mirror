228bddf1565fe095cb54ccee84516b86ea4f1672
Atila Neves <atila.neves@gmail.com>
2020-04-22 14:38:48 +0200

    Test exception when getting rtti for null object

--- tests/ut/rtti/any.d
+++ tests/ut/rtti/any.d
@@ -9,3 +9,15 @@ import mirror.rtti;
 @safe pure unittest {
     auto extended = types!int;
 }
+
+@("rtti.null.object")
+@safe pure unittest {
+    static class Class {}
+    Object o;
+    Class c;
+
+    with(types!Class) {
+        rtti(o).shouldThrowWithMessage("Cannot get RTTI from null object");
+        rtti(c).shouldThrowWithMessage("Cannot get RTTI from null object");
+    }
+}
