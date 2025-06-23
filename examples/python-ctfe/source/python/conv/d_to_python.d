ce25e30df41a8a2068edf39140ad13f16edde93e
Atila Neves <atila.neves@gmail.com>
2025-06-23 18:11:36 +0200

    Python at runtime with ImportC

--- examples/python-ctfe/source/python/conv/d_to_python.d
+++ examples/python-ctfe/source/python/conv/d_to_python.d
@@ -3,9 +3,15 @@ module python.conv.d_to_python;
 
 import python: PyObject;
 import std.traits: isIntegral;
+import std.variant: Variant;
 
 
 PyObject* toPython(T)(T value) if(isIntegral!T) {
     import python: PyLong_FromLong;
     return PyLong_FromLong(value);
+}
+
+PyObject* toPython(Variant variant) {
+    import python: PyLong_FromLong;
+    return PyLong_FromLong(variant.get!long);
 }
---
+++ examples/python-ctfe/source/python/cpython.c
@@ -0,0 +1,4 @@
+#undef __SIZEOF_INT128__
+#pragma attribute(push, nogc, nothrow)
+#include <python3.11/Python.h>
+#pragma attribute(pop)
